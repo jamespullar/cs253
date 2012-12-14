ac59c1de634b7991aa483c345ac9402c271d0e9a
jspullar <jspullar@gmail.com>
2012-12-13 20:13:49 -0800

    added blog with extra styling

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ from google.appengine.ext import db
 env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
                   autoescape=True)
 
-class BaseHandler(webapp2.RequestHandler):
-    def render(self, template, **kw):
-        self.response.out.write(self.render_str(template, **kw))
-    
-    def render_str(self, template, **params):
-        t = env.get_template(template)
-        return t.render(params)
-
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
 # Validators for form data
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
@@ -36,12 +25,28 @@ EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 # Creates a Users object for storing users in db
 class Users(db.Model):
     name = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
     email = db.EmailProperty(required=False)
     created = db.DateTimeProperty(auto_now_add=True)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(self.render_str(template, **kw))
+    
+    def render_str(self, template, **params):
+        t = env.get_template(template)
+        return t.render(params)
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
 class SignUp(BaseHandler):
     def get(self):
@@ -128,10 +133,47 @@ class Welcome(BaseHandler):
         else:
             self.render('signup.html')
 
+class FrontPage(BaseHandler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+
+        self.render("home.html", posts = posts)
+
+class SubmitPage(BaseHandler):
+    def render_page(self, error = "", subject="", content=""):
+        self.render("submit.html", error = error, subject = subject, content = content)
+
+    def get(self):
+        self.render_page()
+
+    def post(self):
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+
+        if subject and content:
+            post = Post(subject = subject, content = content)
+            post.put()
+
+            post_id = post.key().id()
+
+            self.redirect("/%i" % post_id)
+        else:
+            error = "both subject and content required to post"
+            self.render_page(error = error, subject = subject, content = content)
+
+class PermaPage(BaseHandler):
+    def get(self, post_id):
+        post = Post.get_by_id(int(post_id))
+
+        self.render('perma.html', post = post)
+
 app = webapp2.WSGIApplication([('/signup', SignUp),
                                ('/welcome', Welcome),
                                ('/login', Login),
-                               ('/logout', Logout)],
+                               ('/logout', Logout),
+                               ('/', FrontPage),
+                               ('/([0-9]+)', PermaPage),
+                               ('/newpost', SubmitPage)],
                                 debug=True)
 
 def main():
